d20df238a88f646fb1bc63b622a03192b23cc928
Dave Simel <davesimel@gmail.com>
2015-06-01 16:38:44 -0400

    working version without formatting

--- server.rb
+++ server.rb
@@ -1,87 +1,76 @@
 require "sinatra"
+require "pry"
 
 use Rack::Session::Cookie, {
-  secret: "keep_it_secret_keep_it_safe"
+  secret: "secret_cookie!"
 }
 
-gets "/" do
-  erb :index
+GAME_OPTIONS = [ 'Rock', 'Paper', 'Scissors' ]
+WINNING_COMBOS = { "Rock" => "Scissors", "Paper" => "Rock", "Scissors" => "Paper" }
+
+def cpu_output
+  session[:cpu_output] = GAME_OPTIONS.sample
 end
 
-
-class RockPaperScissors
-  attr_accessor :player_input, :cpu_output, :cpu, :player_score, :round
-  WINNING_COMBOS = { r: :s, p: :r, s: :p }
-  RPS_CONVERTER = { r: "ROCK", p: "PAPER", s: "SCISSORS" }
-  GAME_OPTIONS = [ :r, :p, :s ]
-
-  def initialize(player_name = "Player")
-    @player_name = player_name
-    @cpu_score = 0
-    @player_score = 0
-    @round = 1
+def round_winner(player_choice, cpu_choice)
+  if player_choice == cpu_choice
+    session[:winner] = "Tie!"
+    return "Round Winner! Round tied!"
+  elsif WINNING_COMBOS[player_choice] == cpu_choice
+    session[:player_score] += 1
+    session[:winner] = "Player"
+    return "Player Wins! Player threw: #{player_choice} CPU threw: #{cpu_choice}"
+  else
+    session[:computer_score] += 1
+    session[:winner] = "Computer"
+    return "Computer Winner! Player threw: #{player_choice} CPU threw: #{cpu_choice}"
   end
-
-  def cpu_output
-    @cpu_output = GAME_OPTIONS.sample
-  end
-
-  def player_input
-    @player_input = gets.chomp.to_sym
-    until GAME_OPTIONS.include?( @player_input )
-      puts "Please input only 'r', 'p', 's'"
-      player_input
-    end
-  end
-
-  def game_start
-    puts "ROCK, PAPER, SCISSORS: #{@player_name} VS CPU".center(72)
-    puts "First player to win 2 rounds, wins the game!".center(72)
-    puts "SELECT:\n'r' for ROCK\n'p' for PAPER\n's' for SCISSORS"
-    play_game until ( @cpu_score == 2 || @player_score == 2 )
-    puts declare_winner
-  end
-
-  def play_game
-    puts "ROUND #{@round} START! ".center(72)
-    puts "ROCK, PAPER, SCISSORS SAYS... SHOOT! "
-    player_input
-    cpu_output
-
-    puts "===> #{@player_name} CHOSE #{RPS_CONVERTER[@player_input]}!"
-    puts "===> CPU CHOSE #{RPS_CONVERTER[@cpu_output]}!"
-    game_logic
-  end
-
-  def game_logic
-    if @player_input == @cpu_output
-      puts "Round tied!"
-    elsif WINNING_COMBOS[@player_input] == @cpu_output
-      puts "#{@player_name} wins!"
-      @player_score +=1
-    else
-      puts "CPU Wins!"
-      @cpu_score += 1
-    end
-    scoreboard
-  end
-
-  def scoreboard
-    puts "\n Score: CPU #{@cpu_score} - #{@player_name} #{player_score}"
-    puts "------------------------------------------------------------------------\n"
-    @round += 1
-  end
-
-  def declare_winner
-    @cpu_score == 2 ? "CPU is Victorious!" : "Player is Victorious!"
-  end
-
 end
 
-gets "/" do
-
+def game_winner
+  if session[:player_score] == 2
+    session[:player_score] = 0
+    session[:computer_score] = 0
+    session[:winner] = nil
+    return "Game over - Player Wins"
+  elsif session[:computer_score] == 2
+    session[:player_score] = 0
+    session[:computer_score] = 0
+    session[:winner] = nil
+    return "Game over - Computer wins"
+  else
+    return "First to 2 wins!"
+  end
 end
 
+get "/" do
+  redirect "/mmorps"
+end
 
-dave = RockPaperScissors.new("ZAC")
-dave.game_start
+get "/mmorps" do
+  if session[:player_score].nil? || session[:computer_score].nil? || session[:winner].nil?
+    session[:player_score] = 0
+    session[:computer_score] = 0
+    session[:winner] = nil
+    session[:player_choice] = nil
+    session[:cpu_output] = nil
+  else
+    game_summary = "CPU threw: #{session[:cpu_output]} - Player threw: #{session[:player_choice]} --> \n Round winner: #{session[:winner]}"
+  end
+
+  erb :index, locals: { game_summary: game_summary }
+end
+
+post "/mmorps" do
+  session[:player_choice] = params[:player_choice]
+
+  round_winner(session[:player_choice], cpu_output)
+  # puts results
+  # if results == "Player Wins!"
+  #   session[:player_score] += 1
+  # elsif results == "Computer Winner!"
+  #   session[:computer_score] += 1
+  # end
+
+  redirect "/mmorps"
+end
